2b05d0d63a7a84e7d10fc8696131b819ebc82be6
Francisco Carlos Calderon <nestor_05ribero@hotmail.com>
2022-03-03 14:14:18 -0500

    Create

---
+++ mainApp.py
@@ -0,0 +1,32 @@
+from flask import Flask,render_template
+
+
+app=Flask(__name__)
+
+
+
+@app.route("/play")
+def play():
+    return render_template("index.html",boxes=3,color="blue")
+
+
+@app.route("/play/<num>")
+def num(num):
+    return render_template("index.html",boxes=int(num),color="blue")
+
+
+@app.route("/play/<num>/<colors>")
+def color(num,colors):
+    print(colors)
+    if(colors!="red" and colors!="blue" and colors!="green"):
+        print("Error")
+        return render_template("index.html",boxes=int(num),color="blue") 
+    return render_template("index.html",boxes=int(num),color=colors)
+
+
+
+
+
+
+if(__name__=="__main__"):
+    app.run(debug=True)
